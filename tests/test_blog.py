5e5df55fabc6363a6d299669315a8a98e26111e4
wangechimk <wangechik.michelle@gmail.com>
2021-05-02 23:30:29 +0300

    add user test and blog tests

---
+++ tests/test_blog.py
@@ -0,0 +1,27 @@
+import unittest
+from app.models import Blog, User
+from app import db
+
+
+class BlogModelTest(unittest.TestCase):
+    def setUp(self):
+        self.user_wangechi = User(username='wangechi', password='Pass', email='wangechik@gmail.com')
+        self.new_blog = Blog(id=1, title='Test', content='This is a test blog', user_id=self.user_wangechi.id)
+
+    def tearDown(self):
+        Blog.query.delete()
+        User.query.delete()
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_blog.title, 'Test')
+        self.assertEquals(self.new_blog.content, 'This is a test blog')
+        self.assertEquals(self.new_blog.user_id, self.user_wangechi.id)
+
+    def test_save_blog(self):
+        self.new_blog.save()
+        self.assertTrue(len(Blog.query.all()) > 0)
+
+    def test_get_blog(self):
+        self.new_blog.save()
+        got_blog = Blog.get_blog(1)
+        self.assertTrue(get_blog is not None)
---
+++ tests/user_test.py
@@ -0,0 +1,18 @@
+import unittest
+
+from app.models import User
+
+
+class UserTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User(username='wangechi', email="wangechik@gmail.com", password='Pass')
+
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.hashed_passwordis, not None)
+
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.hashed_password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('qwerty'))
