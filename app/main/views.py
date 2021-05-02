fcec05ce932ff2d473423121c0f16bf02cec6fef
wangechimk <wangechik.michelle@gmail.com>
2021-05-02 22:43:25 +0300

    resize image uploaded by user

--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,9 @@ from app.models import User, Blog, Comment, Subscriber
 from .forms import UpdateProfile, CreateBlog
 from .. import db
 from ..email import mail_message
+import secrets
+import os
+from PIL import Image
 
 
 @main.route('/')
@@ -14,14 +17,38 @@ def index():
     return render_template('index.html', blogs=blogs)
 
 
-@main.route('/profile/<name>')
-@login_required
-def profile(name):
-    user = User.query.filter_by(username=name).first()
-    if user == None:
-        abort(404)
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex + f_ext
+    picture_path = os.path.join('app/static/photos', picture_filename)
+    output_size = (150, 150)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_filename
 
-    return render_template('profile/profile.html', user=user)
+
+@main.route('/profile', methods=['POST', 'GET'])
+@login_required
+def profile():
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        if form.profile_picture.data:
+            picture_file = save_picture(form.profile_picture.data)
+            current_user.profile_pic_path = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        current_user.bio = form.bio.data
+        db.session.commit()
+        flash('Succesfully updated your profile')
+        return redirect(url_for('main.profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        form.bio.data = current_user.bio
+    profile_pic_path = url_for('static', filename='photos/' + current_user.profile_pic_path)
+    return render_template('profile/profile.html', profile_pic_path=profile_pic_path, form=form)
 
 
 @main.route('/user/<name>/updateprofile', methods=['POST', 'GET'])
