25d82b30164c55268292e756420e8e2e73604827
wangechimk <wangechik.michelle@gmail.com>
2021-05-02 22:27:38 +0300

    add validators to the register form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,24 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-
+from wtforms import ValidationError
 from app.models import User
 
 
-def validate_email(email):
-    user = User.query.filter_by(email=email.data).first()
-    if user:
-        raise ValidationError('The email is already taken..please choose a new one')
+def validate_email(self, data_field):
+    if User.query.filter_by(email=data_field.data).first():
+        raise ValidationError(message="The Email has already been taken!")
 
-
-def validate_username(username):
-    user = User.query.filter_by(username=username.data).first()
-    if user:
-        raise ValidationError('The username is already taken..please choose a new one')
+    def validate_username(self, data_field):
+        if User.query.filter_by(username=data_field.data).first():
+            raise ValidationError(message="The username has already been taken")
 
 
 class RegisterForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField('Enter Your Username', validators=[Required(), Length(min=4, max=20)])
     email = StringField('email', validators=[DataRequired(), Email()])
     password = PasswordField('password', validators=[DataRequired()])
     confirm_password = PasswordField('confirm_password', validators=[DataRequired(), EqualTo('password')])
