ae170baeddb03950655db46ae557c633040f4a41
wangechimk <wangechik.michelle@gmail.com>
2021-05-02 17:49:06 +0300

    create new form class for handling new posts

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,6 @@
-from flask_login import current_user
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField
-from wtforms.validators import Required, DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import Required
 
 from app.models import User
 
@@ -11,47 +9,8 @@ class UpdateProfile(FlaskForm):
     bio = TextAreaField('Write a brief bio about you.', validators=[Required()])
     submit = SubmitField('Save')
 
-    def validate_username(self, username):
-        if username.data != current_user.username:
-            user = User.query.filter_by(username=username.data).first()
-            if user:
-                raise ValidationError('That username is taken. Please choose a different one.')
 
-    def validate_email(self, email):
-        if email.data != current_user.email:
-            user = User.query.filter_by(email=email.data).first()
-            if user:
-                raise ValidationError('That email is taken. Please choose a different one.')
-
-
-class RequestResetForm(FlaskForm):
-    # email = StringField('Email',
-    #                     validators=[DataRequired(), Email()])
-    submit = SubmitField('Request Password Reset')
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is None:
-            raise ValidationError('There is no account with that email. You must register first.')
-
-
-class ResetPasswordForm(FlaskForm):
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Reset Password')
-
-
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
+class CreateBlog(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    content = TextAreaField('Blog Content', validators=[Required()])
     submit = SubmitField('Post')
-
-
-class CommentForm(FlaskForm):
-    comment = TextAreaField('Comment', validators=[DataRequired()])
-    submit = SubmitField('Post')
-
-
-class Vote(FlaskForm):
-    submit = SelectField('Like')
