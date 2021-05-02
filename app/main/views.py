21c15c368ff50c3cf773ab5a3d641210e0f04a3e
wangechimk <wangechik.michelle@gmail.com>
2021-05-02 20:19:04 +0300

    create route to update the blog

--- app/main/views.py
+++ app/main/views.py
@@ -9,7 +9,7 @@ from ..email import mail_message
 
 @main.route('/')
 def index():
-    blogs = Blog.query.order_by(Blog.posted.desc()).limit(3).all()
+    blogs = Blog.query.order_by(Blog.posted.desc()).limit(10).all()
     return render_template('index.html', blogs=blogs)
 
 
@@ -75,3 +75,28 @@ def subscribe():
                  new_subscriber=new_subscriber)
     flash('Sucessfuly subscribed')
     return redirect(url_for('main.index'))
+
+
+@main.route('/blog/<id>')
+def blog(id):
+    blog = Blog.query.get(id)
+    return render_template('blog.html', blog=blog)
+
+
+@main.route('/blog/<blog_id>/update', methods=['GET', 'POST'])
+@login_required
+def updateblog(blog_id):
+    blog = Blog.query.get(blog_id)
+    if blog.user != current_user:
+        abort(403)
+    form = CreateBlog()
+    if form.validate_on_submit():
+        blog.title = form.title.data
+        blog.content = form.content.data
+        db.session.commit()
+        flash("You have updated your Blog!")
+        return redirect(url_for('main.blog', id=blog.id))
+    if request.method == 'GET':
+        form.title.data = blog.title
+        form.content.data = blog.content
+    return render_template('newblog.html', form=form)
