19b1e4d8184d9d8c2fdda311bf746924ac7149bb
wangechimk <wangechik.michelle@gmail.com>
2021-05-02 18:01:26 +0300

    create a vie method to handle creation of a new blog  and display posts

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,15 @@
 from flask import render_template, request, redirect, url_for, abort, flash
-from flask_login import current_user, login_required
+from flask_login import current_user, login_required, current_user
 from app.main import main
-from app.models import User
-from .forms import UpdateProfile
+from app.models import User, Blog
+from .forms import UpdateProfile, CreateBlog
 from .. import db
 
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    blogs = Blog.query.order_by(Blog.posted.desc()).limit(3).all()
+    return render_template('index.html', blogs=blogs)
 
 
 @main.route('/profile/<name>')
@@ -33,3 +34,17 @@ def updateprofile(name):
         user.save()
         return redirect(url_for('.profile', name=name))
     return render_template('profile/updateprofile.html', form=form)
+
+
+@main.route('/new_post', methods=['POST', 'GET'])
+@login_required
+def new_blog():
+    form = CreateBlog()
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+        user_id = current_user._get_current_object().id
+        blog = Blog(title=title, content=content, user_id=user_id)
+        blog.save()
+        return redirect(url_for('main.index'))
+    return render_template('newblog.html', form=form)
