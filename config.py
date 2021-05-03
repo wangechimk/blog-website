8b4fe9d491f1cbca570a86e03e9920f7676bd924
wangechimk <wangechik.michelle@gmail.com>
2021-05-03 22:50:10 +0300

    second deployment

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI',
                                              'postgresql+psycopg2://michellewangechi:Rosemary70@localhost/blogwebsite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY', '1234key')
